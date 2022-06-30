d1033c70053c74611358d271dfce7af0e8ee174a
John Chou <jochou02@gmail.com>
2022-06-29 20:30:55 -0700

    Added Card doctests

--- card.py
+++ card.py
@@ -1,5 +1,32 @@
 class Card:
 
+    """
+    # Card class Doctests #
+
+    >>> card_1 = Card("A", "Spades")
+    >>> print(card_1)
+    A of Spades
+    >>> card_1
+    A of Spades
+
+    >>> card_2 = Card("K", "Spades")
+    >>> print(card_2)
+    K of Spades
+    >>> card_2
+    K of Spades
+
+    >>> card_3 = Card("A", "Diamonds")
+
+    # Comparisons #
+
+    >>> card_1 < card_2
+    False
+    >>> card_1 > card_2
+    True
+    >>> card_3 > card_1
+    False
+    """
+
     def __init__(self, rank, suit):
         assert isinstance(rank, (str, int)) and isinstance(suit,str)
         self.rank = rank
@@ -8,7 +35,7 @@ class Card:
     def __lt__(self, other_card):
         rank_order = '2345678910JQKA'
         if self.rank == other_card.rank:
-            suit_order = 'clubsdiamondsheartsspades'
+            suit_order = 'DiamondsClubsHeartsSpades'
             return suit_order.find(self.suit) < suit_order.find(other_card.suit)
         return rank_order.find(str(self.rank)) < rank_order.find(str(other_card.rank))
 
---
+++ driver.py
@@ -0,0 +1,13 @@
+# File to test functionalty #
+
+from card import Card
+
+card_1 = Card("A", "spades")
+print(card_1)
+
+"""
+Given two integers, return the sum.
+
+>>> add(2, 3)
+5
+"""
